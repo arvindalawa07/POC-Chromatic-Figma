a5fb03436ce99c740e32721de9a37f28f946bf95
arvindalawa07 <arvindalawa1998@gmail.com>
2024-12-17 18:44:16 +0530

    remove addidtion text from button

--- src/stories/button.component.ts
+++ src/stories/button.component.ts
@@ -11,7 +11,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   [ngClass]="classes"
   [ngStyle]="{ 'background-color': backgroundColor }"
 >
-  {{ label }}test
+  {{ label }}
 </button>`,
   styleUrls: ['./button.css'],
 })
